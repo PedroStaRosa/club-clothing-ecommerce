2564852a9db30a95bd7f5c9085f085c1cf36da50
PedroStaRosa <pedro.santarosa@outlook.com>
2023-01-06 21:01:46 -0300

    feat: show CategoryItem for each Category

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react'
 /* import axios from 'axios' */
+
+// Components
+import CategoryItem from '../category-item/category-item.component'
+
 // Utilities
 import Category from '../../types/category.types'
 /* import env from '../../config/env.config' */
@@ -13,6 +17,7 @@ const Categories = () => {
   const fetchCategories = async () => {
     try {
       /* const { data } = await axios.get(`${env.apiUrl}/api/category`) */
+      // Categories HardCode - Temporary
       const data = [
         {
           id: 'cat01',
@@ -62,7 +67,13 @@ const Categories = () => {
   }, [])
   return (
     <div className="categories-container">
-      <div className="categories-content"></div>
+      <div className="categories-content">
+        {categories.map((category) => (
+          <div key={category.id}>
+            <CategoryItem category={category} />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
