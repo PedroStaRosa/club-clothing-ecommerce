96a38c785050a3516d8b1f28caf86ed7158a6bb4
PedroStaRosa <pedro.santarosa@outlook.com>
2023-01-08 09:52:44 -0300

    feat: add ThemeColors file

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,18 @@
+const Colors = {
+  primary: '#212529',
+  text: {
+    white: '#f8f9fa',
+    black: '#343a40'
+  },
+  background: {
+    dark: '#212529',
+    white: '#f8f9fa'
+  },
+  input: {
+    background: '#E9ECEF',
+    placeholder: '6C757D'
+  },
+  erro: '#ff6a6a'
+}
+
+export default Colors
